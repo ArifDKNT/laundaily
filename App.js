89632d767d115a2ceb0fab5713b34b261a5d675a
Arif Dikkanat <arifdikkanat2000@gmail.com>
2023-10-12 14:25:11 +0300

    Laundy app basic template

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import StackNavigator from "./StackNavigator";
+import store from "./store";
+
+export default function App() {
+  return (
+    <>
+      <StackNavigator />
+      <StatusBar style="auto" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
---
+++ screens/OrderScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const OrderScreen = () => {
+  return (
+    <Text>OrderScreen</Text>
+  )
+}
+
+export default OrderScreen
