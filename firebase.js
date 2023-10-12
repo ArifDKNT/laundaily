193db3a3bd3cc08d95f7ceb9dc670353365d5f4a
Arif Dikkanat <arifdikkanat2000@gmail.com>
2023-10-12 15:14:39 +0300

    Login anf Firebase config

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6Irh0Xs_Jrt_6IOUM2mz6vxZm3q52HoY",
-  authDomain: "laundry-application-7f4c7.firebaseapp.com",
-  projectId: "laundry-application-7f4c7",
-  storageBucket: "laundry-application-7f4c7.appspot.com",
-  messagingSenderId: "571088013444",
-  appId: "1:571088013444:web:6a5e8ded1f44846994dad1"
+  apiKey: "AIzaSyCQumkaKbK5FN92D15SIYtRGtvr7Ig-TLQ",
+  authDomain: "laundaily-3d101.firebaseapp.com",
+  projectId: "laundaily-3d101",
+  storageBucket: "laundaily-3d101.appspot.com",
+  messagingSenderId: "271773258056",
+  appId: "1:271773258056:web:df508be1de87c42a25637d",
+  measurementId: "G-Q67X350QCY"
 };
 
 
